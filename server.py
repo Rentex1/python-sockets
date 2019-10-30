1f1a213c15adf794e6296855f4e94028e0e2ef4f
Rentex1 <luis.stauss@gmx.de>
2019-10-30 18:55:10 +0100

    server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ HOST = '0.0.0.0'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((HOST, PORT)) #bind the port
     s.listen(1)
     conn, addr = s.accept()
     with conn:
